75111b0829c75e676bb5ebc9bac7b7af2b081268
CarlosRPEvertsz <carlosrpevertsz@gmail.com>
2020-07-31 23:13:24 -0400

    Add project files.

---
+++ TareaNo1_4_Problema3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaNo1_4_Problema3
+{
+    public class Club
+    {
+        static void Main(string[] args)
+        {
+            clsSocio[] Socio = new clsSocio[3];
+            Socio[0] = new clsSocio("José Peguero", 12);
+            Socio[1] = new clsSocio("Carlos Ureña", 18);
+            Socio[2] = new clsSocio("Pedro Bobea", 15);
+
+            int IndiceMayorAntiguedad = 0;
+            int MayorAntiguedad = 0;
+
+            for (int i = 0; i <= 2; i++)
+            {
+                if (Socio[i].RetornarAntiguedad() > MayorAntiguedad)
+                {
+                    IndiceMayorAntiguedad = i;
+                    MayorAntiguedad = Socio[i].RetornarAntiguedad();
+                }
+
+            }
+            Console.WriteLine($"El Socio '{Socio[IndiceMayorAntiguedad].RetornarNombre()}' tiene mayor antiguedad en el club.");
+        }
+    }
+}
---
+++ TareaNo1_4_Problema3/clsSocio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaNo1_4_Problema3
+{
+    public class clsSocio
+    {
+        private string Nombre;
+        private int Antiguedad;
+
+        public clsSocio(string NombreSocio, int AntiguedadSocio)
+        {
+            Nombre = NombreSocio;
+            Antiguedad = AntiguedadSocio;
+        }
+        public int RetornarAntiguedad()
+        {
+            return Antiguedad;
+        }
+        public string RetornarNombre()
+        {
+            return Nombre;
+        }
+
+    }
+}
